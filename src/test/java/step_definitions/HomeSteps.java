d1231d82fb7d370038025087e82c2af2752bf5cb
Majid Sodavi <majidsoda@gmail.com>
2023-05-03 11:32:58 -0400

    updated AS-3

--- src/test/java/step_definitions/HomeSteps.java
+++ src/test/java/step_definitions/HomeSteps.java
@@ -1,29 +1,32 @@
 package step_definitions;
 
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
 import pages.HomePage;
 import utils.BrowserUtils;
 
 import java.util.List;
+import java.util.Map;
 
 public class HomeSteps {
     HomePage page;
-    public HomeSteps() { page = new HomePage(); }
+
+    public HomeSteps() {
+        page = new HomePage();
+    }
+
     @Then("the following should be displayed:")
-    public void i_should_see_header(List<String> button) {
-        for (String each : button) {
-            switch (each) {
-                case "10090 Main Street":
-                    BrowserUtils.click(page.addressLine1);
-                    break;
-                case "Fairfax, VA, USA":
-                    BrowserUtils.click(page.addressLine2);
-                    break;
-                case "+1 703-831-3227":
-                    BrowserUtils.click(page.phoneNumber);
-                    break;
-            }
-        }
+    public void i_should_see_header(Map<String, String> expectedValues) {
+        BrowserUtils.assertEquals(expectedValues.get("address1"), page.addressLine1.getText());
+        BrowserUtils.isDisplayed(page.addressLine1);
+
+        BrowserUtils.assertEquals(expectedValues.get("address2"), page.addressLine2.getText());
+        BrowserUtils.isDisplayed(page.addressLine2);
+
+        BrowserUtils.assertEquals(expectedValues.get("phoneNumber"), page.phoneNumber.getText());
+        BrowserUtils.isDisplayed(page.phoneNumber);
+
+        System.out.println("expectedValues.toString() = " + expectedValues.toString());
     }
 
 }
--- src/test/resources/features/homepage.feature
+++ src/test/resources/features/homepage.feature
@@ -4,6 +4,6 @@ Feature: Homepage Scenarios
   @AS-3
   Scenario: Verify our address and phone number under the main navigation bar on top of the page is displayed.
     Then the following should be displayed:
-    |10090 Main Street|
-    |Fairfax, VA, USA|
-    |+1 703-831-3227|
+    |address1|10090 Main Street|
+    |address2|Fairfax, VA, USA|
+    |phoneNumber|+1 703-831-3227|
