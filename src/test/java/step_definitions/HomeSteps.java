834c91f3acb710af4605ae035f88099205debcfe
DariaTrabsky <daria.trabsky@gmail.com>
2023-05-04 21:45:20 -0400

    AS-6

--- src/test/java/step_definitions/HomeSteps.java
+++ src/test/java/step_definitions/HomeSteps.java
@@ -1,6 +1,9 @@
 package step_definitions;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.junit.Assert;
 import pages.HomePage;
 import utils.BrowserUtils;
@@ -32,6 +35,49 @@ public class HomeSteps {
     @Then("verify title of the homepage is {string}")
     public void verifyTitleOfTheHomepageIs(String title) {
         String actual = BrowserUtils.getDriver().getTitle();
-        BrowserUtils.assertEquals(actual,title);
+        BrowserUtils.assertEquals(actual, title);
+    }
+
+
+    @Given("I am on the {string} page")
+    public void iAmOnTheHomepage(String titleMain) {
+        String actual = BrowserUtils.getDriver().getTitle();
+        BrowserUtils.assertEquals(actual, titleMain);
+    }
+
+    @When("I look at the navigation bar")
+    public void iLookAtTheNavigationBar() {
+        BrowserUtils.isDisplayed(page.navBar);
+    }
+
+    @Then("verify the following nav button are displayed:")
+    public void verifyTheFollowingIsDisplayed(List<String> navButtonName) {
+        for (String each : navButtonName) {
+            switch (each) {
+                case "Get Support":
+                    BrowserUtils.isDisplayed(page.getSupportBtn);
+                    break;
+                case "Job Career":
+                    BrowserUtils.isDisplayed(page.jobCareerBtn);
+                    break;
+                case "Feedback":
+                    BrowserUtils.isDisplayed(page.feedbackBtn);
+                    break;
+                case "English":
+                    BrowserUtils.isDisplayed(page.englishNavBtn);
+                    BrowserUtils.click(page.englishNavBtn);
+                    BrowserUtils.isDisplayed(page.englishBtn);
+                    BrowserUtils.isDisplayed(page.spanishBtn);
+                    BrowserUtils.isDisplayed(page.frenchBtn);
+                    break;
+                default:
+                    System.out.println("Button doesn't exist");
+
+            }
+        }
     }
 }
+
+
+
+
