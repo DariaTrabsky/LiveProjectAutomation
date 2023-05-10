4a30d41ffffeec646a107f0ae7c2177dea6fdbf8
Majid Sodavi <majidsoda@gmail.com>
2023-05-09 20:39:08 -0400

    AS-23

--- src/test/java/runners/apiRunner.java
+++ src/test/java/runners/apiRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         glue = "step_definitions/api_steps",
         stepNotifications = true,
         dryRun = false
-        ,tags = "@api"
+        ,tags = "@AS-23"
 )
 public class apiRunner {
 
