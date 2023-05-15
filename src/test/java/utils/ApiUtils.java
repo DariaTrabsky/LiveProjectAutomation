567fb11d1efb8260ccef4d1cf7f0101c3bc561de
Majid Sodavi <majidsoda@gmail.com>
2023-05-15 13:08:22 -0400

    a

--- src/test/java/utils/ApiUtils.java
+++ src/test/java/utils/ApiUtils.java
@@ -5,7 +5,6 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 
-import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
